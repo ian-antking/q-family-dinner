dfbb412c275ade89c230eb41b406b3593ad9db91
Ian King <ian.antking@gmail.com>
2019-05-30 12:33:25 +0100

    added background gradient to hero image

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import LazyHero from 'react-lazy-hero';
 import { Heading, Box } from 'react-bulma-components/full';
 
+const prideGradient = 'linear-gradient(180deg, #f00000, #f00000 16.67%, #ff8000 16.67%, #ff8000 33.33%, #ffff00 33.33%, #ffff00 50%, #007940 50%, #007940 66.67%, #4040ff 66.67%, #4040ff 83.33%, #a000c0 83.33%, #a000c0)';
+
 const HomePage = () => (
   <LazyHero
-    imageSrc="https://unsplash.it/2000/1000"
     minHeight="100vh"
+    opacity={0.5}
+    style={
+      {
+        background: prideGradient,
+        overflow: 'hidden',
+      }
+    }
   >
     <Box
       style={
