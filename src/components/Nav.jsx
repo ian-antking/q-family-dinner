ad2e57f6871bdadb76da58a6d88a6eaadbdd17ab
Ian King <ian.antking@gmail.com>
2019-05-30 11:08:00 +0100

    added event handlers for menu bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,52 @@
 import React from 'react';
 import { Navbar } from 'react-bulma-components/full';
 
-const Nav = () => (
-  <Navbar />
-);
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
+  _handleMenuClick() {
+    this.setState({
+      open: !this.state.open,
+    });
+  }
+
+  render() {
+    return (
+      <Navbar
+        fixed="top"
+      >
+        <Navbar.Brand>
+          <Navbar.Item>
+            <h1>Queer Family Tea</h1>
+          </Navbar.Item>
+          <Navbar.Burger
+            active="open"
+            onClick={() => this._handleMenuClick()}
+          />
+        </Navbar.Brand>
+        <Navbar.Menu
+          active="open"
+          className={this.state.open ? 'is-active' : null}
+        >
+          <Navbar.Container
+            position="end"
+          >
+            <Navbar.Item>
+              <h2>About</h2>
+            </Navbar.Item>
+            <Navbar.Item>
+              <h2>Events</h2>
+            </Navbar.Item>
+          </Navbar.Container>
+        </Navbar.Menu>
+      </Navbar>
+    );
+  }
+}
 
 export default Nav;
