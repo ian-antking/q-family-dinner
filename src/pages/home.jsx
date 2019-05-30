23e672659f45ec27430f36e9a38c256b1e390f21
Ian King <ian.antking@gmail.com>
2019-05-30 12:51:40 +0100

    moved gradient into asset file

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import LazyHero from 'react-lazy-hero';
 import { Heading, Box } from 'react-bulma-components/full';
-
-const prideGradient = 'linear-gradient(180deg, #f00000, #f00000 16.67%, #ff8000 16.67%, #ff8000 33.33%, #ffff00 33.33%, #ffff00 50%, #007940 50%, #007940 66.67%, #4040ff 66.67%, #4040ff 83.33%, #a000c0 83.33%, #a000c0)';
+import prideGradient from '../assets/pride-gradient';
 
 const HomePage = () => (
   <LazyHero
