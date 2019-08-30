20b4ae5a8527ca293acd37b709377fad3cdff0e3
Ian King <ian.antking@gmail.com>
2019-08-30 13:46:04 +0100

    styled social buttons

--- src/components/contact-info.jsx
+++ src/components/contact-info.jsx
@@ -6,9 +6,7 @@ import {
 } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
 import {
-  Section,
   Content,
-  Heading,
   Container,
 } from 'react-bulma-components/full';
 
@@ -16,7 +14,6 @@ import '../styles/contact-info.scss';
 
 const ContactInfo = () => (
   <Content>
-    <Heading>Us</Heading>
     <Container className="contact-card">
       <div className="link-list">
         <a href="https://www.facebook.com/Queer-Family-Tea-638643326624464">
@@ -25,7 +22,7 @@ const ContactInfo = () => (
         <a href="https://www.instagram.com/queerfamilytea/">
           <FontAwesomeIcon icon={faInstagram} />
         </a>
-        <a href="info@queerfamilytea.com">
+        <a href="mailto:info@queerfamilytea.com">
           <FontAwesomeIcon icon={faEnvelopeSquare} />
         </a>
       </div>
