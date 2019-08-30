28a3af45ff4c943303b6d6adc8843491147c45f0
Ian King <ian.antking@gmail.com>
2019-08-30 13:42:04 +0100

    added social buttons

--- src/components/contact-info.jsx
+++ src/components/contact-info.jsx
@@ -12,13 +12,23 @@ import {
   Container,
 } from 'react-bulma-components/full';
 
+import '../styles/contact-info.scss';
 
 const ContactInfo = () => (
   <Content>
-    <Container>
-      <FontAwesomeIcon icon={faFacebookSquare} />
-      <FontAwesomeIcon icon={faInstagram} />
-      <FontAwesomeIcon icon={faEnvelopeSquare} />
+    <Heading>Us</Heading>
+    <Container className="contact-card">
+      <div className="link-list">
+        <a href="https://www.facebook.com/Queer-Family-Tea-638643326624464">
+          <FontAwesomeIcon icon={faFacebookSquare} />
+        </a>
+        <a href="https://www.instagram.com/queerfamilytea/">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href="info@queerfamilytea.com">
+          <FontAwesomeIcon icon={faEnvelopeSquare} />
+        </a>
+      </div>
     </Container>
   </Content>
 );
