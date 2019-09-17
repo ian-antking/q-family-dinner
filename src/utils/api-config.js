493e1b08f05be0507a6072ea74da5cd0a906c985
Ian King <ian.antking@gmail.com>
2019-09-17 18:35:06 +0100

    switched back to live api

--- src/utils/api-config.js
+++ src/utils/api-config.js
@@ -1,5 +1,5 @@
-// const apiString = 'https://q-family-dinner.herokuapp.com';
-const apiString = 'http://localhost:4000';
+const apiString = 'https://q-family-dinner.herokuapp.com';
+// const apiString = 'http://localhost:4000';
 
 const googleKey = 'AIzaSyAF8ycsjyy9eeofTAP0lB3U6_a2_LXcB68';
 const instagramUsername = 'queerfamilytea';
