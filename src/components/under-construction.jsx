c0f9efa5313a7de42fad07c84c67ba31972f11e5
Ian King <ian.antking@gmail.com>
2020-03-10 12:46:09 +0000

    articles page shows under constructon if no articles

--- src/components/under-construction.jsx
+++ src/components/under-construction.jsx
@@ -4,6 +4,7 @@ import { Heading, Image } from 'react-bulma-components/full';
 const UnderConstruction = () => (
   <React.Fragment>
     <Heading>Under Construction</Heading>
+    <Heading subtitle>Check back soon for new content!</Heading>
     <Image src="src/assets/undraw_under_construction_46pa.svg" />
   </React.Fragment>
 );
--- src/containers/articles.jsx
+++ src/containers/articles.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import UnderConstruction from '../components/under-construction';
 
-const ArticlesPage = () => {
+const ArticlesPage = (props) => {
+  const pages = props.pages.filter(page => page.fields.discoverable);
   return (
-    <UnderConstruction />
+    !pages.length && <UnderConstruction />
   );
 };
 
