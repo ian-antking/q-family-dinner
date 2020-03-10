5d503058194912a8177108c04cd584a39ac27cde
Ian King <ian.antking@gmail.com>
2020-03-10 14:49:48 +0000

    articles page can filter articles by tag

--- src/containers/articles.jsx
+++ src/containers/articles.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import UnderConstruction from '../components/under-construction';
+import PageDiscovery from '../components/page-discovery';
+import queryString from 'query-string';
 
 const ArticlesPage = (props) => {
-  const pages = props.pages.filter(page => page.fields.discoverable);
+  const query = props.location.search && JSON.parse(queryString.parse(props.location.search).query);
+  const pages = query.tag ? (
+    props.pages.filter(page => {
+      return page.fields.tags && page.fields.tags.includes(query.tag);
+    })
+  ) : (
+    props.pages
+  );
+
   return (
-    !pages.length && <UnderConstruction />
+    pages.length ? <PageDiscovery pages={pages} /> : <UnderConstruction />
   );
 };
 
