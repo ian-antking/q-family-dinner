2821219ee584de720197a0376b76855c03cde1f4
GummiCode <gummicodeblog@gmail.com>
2020-08-01 20:10:50 +0100

    Branched repo. Cloned. Removed all content and replaced with a new React app.

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import 'raf/polyfill';
-import React from 'react';
-import { render } from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
-import App from './app';
-
-render(<Router><App /></Router>, document.getElementById('root'));
