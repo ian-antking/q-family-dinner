e7e72d3daf9f8d14c1ee1b46dee70ea9a35a4170
ian-antking <ian.antking@gmail.com>
2020-08-02 20:53:05 +0100

    get content from cms and store in state

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "styled-components";
 import themes from "../styles/themes";
+import getContent from "../services/get-content";
 
 function App() {
+  const [content, setContent] = useState({})
+
+  useEffect(() => {
+    (async () => {
+      const data = await getContent()
+      setContent(data);
+    })()
+  }, [])
+
   return (
     <>
       <ThemeProvider theme={themes.default}>
