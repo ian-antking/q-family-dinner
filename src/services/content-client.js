34c9a2eeb58783351fb8ffc363b56f1144f0ded8
ian-antking <ian.antking@gmail.com>
2020-08-17 16:10:34 +0100

    fetch content from test environment while in development

--- src/services/content-client.js
+++ src/services/content-client.js
@@ -1,7 +1,13 @@
 import { createClient } from "contentful";
 import { contentful } from "../config/api";
 
+const environments = {
+  development: 'test',
+  production: 'master'
+}
+
 export default createClient({
   space: contentful.contentSpace,
   accessToken: contentful.contentToken,
+  environment: environments[process.env.NODE_ENV],
 });
