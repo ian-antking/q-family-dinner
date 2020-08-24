92e598c092892b16f63523c73268a11f6e640fb5
GummiCode <43494071+GummiCode@users.noreply.github.com>
2020-08-24 16:56:32 +0100

    Created a Navbar component consisting of a basic fixed navbar with no links.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import themes from '../styles/themes';
 import getContent from '../services/get-content';
+import Navbar from  './Navbar';
 
 function App() {
   const [content, setContent] = useState({});
@@ -16,6 +17,7 @@ function App() {
   return (
     <>
       <ThemeProvider theme={themes.default}>
+        <Navbar />
         <header>
           <p>Queer Family Tea</p>
         </header>
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledNavbar = styled.div`
+position: fixed;
+top: 0;
+left: 0;
+height: 60px;
+width: 100vw;
+color: #ffffff;
+background: #000000;
+display: flex;
+flex-flow: row nowrap;
+justify-content: space-between;
+align-items: center;
+z-index: 9;
+`;
+
+
+const NavBar = () => {
+	return (
+		<StyledNavbar>
+
+
+		</StyledNavbar>
+	);
+	};
+
+export default NavBar;
+
