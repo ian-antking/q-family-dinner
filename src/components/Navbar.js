d9ad29ca97fc2f7ef462030d5ad2c1dfb016463d
GummiCode <43494071+GummiCode@users.noreply.github.com>
2020-08-25 05:49:38 +0100

    Added a home/page title link to the navbar. Restyled the navbar to match the original navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,17 +6,23 @@ const StyledNavbar = styled.div`
 position: fixed;
 top: 0;
 left: 0;
-height: 60px;
-width: 100vw;
-color: #ffffff;
-background: #000000;
+height: 50px;
+width: calc(100vw - 30px);
+padding: 0 15px;
+color: #000000;
+background: whitesmoke;
 display: flex;
 flex-flow: row nowrap;
 justify-content: space-between;
 align-items: center;
 z-index: 9;
 
-
+	a {
+		font-size: 1.8em;
+		font-weight: bold;
+		color: #000000;
+		text-decoration: none;
+	}
 `;
 
 
