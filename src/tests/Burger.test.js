a5964bd9970796754975eefef0780d30befeb409
GummiCode <43494071+GummiCode@users.noreply.github.com>
2020-09-06 11:22:01 +0100

    Added onClick function call test for the Burger component.

--- src/tests/Burger.test.js
+++ src/tests/Burger.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { HashRouter as Router } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Burger from '../components/library/BurgerMenuComponents/Burger';
 
-const open = false;
+const closed = false;
+const open = true;
 const setOpen = jest.fn();
 
 test('snapshot: Burger component', () => {
   const { asFragment } = render(
-    <Burger open={open} setOpen={setOpen} />
+    <Burger open={closed} setOpen={setOpen} />
   );
   const component = asFragment();
   expect(component).toMatchSnapshot();
@@ -16,8 +17,17 @@ test('snapshot: Burger component', () => {
 
 test('Burger component renders', () => {
   const { getByTestId } = render(
-    <Burger open={open} setOpen={setOpen} />
+    <Burger open={closed} setOpen={setOpen} />
   );
   const component = getByTestId('burger');
   expect(component).toBeInTheDocument();
+});
+
+test('The function passed to onClick is called on click.', () => {
+  const { getByTestId } = render (
+    <Burger open={closed} setOpen={setOpen} />
+  );
+  const component = getByTestId('burger');
+  fireEvent.click(component);
+  expect(setOpen).toHaveBeenCalled();
 });
