38f01b206a501d7e1618d299199d88b7e7e40be8
GummiCode <gummicodeblog@gmail.com>
2020-09-28 19:54:40 +0100

    Added prop types and defualt props to Article component.

--- src/components/library/Article.js
+++ src/components/library/Article.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { INLINES, BLOCKS } from '@contentful/rich-text-types';
 import ArticleCard from './ArticleCard';
-
 
 const ArticleStyles = styled.div`
 	padding: 0 20px;
@@ -29,56 +28,53 @@ const ArticleStyles = styled.div`
     height: 1px;
     background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0));
 	}
-
-
-`
-
-const IMAGES = [
-  'image/png',
-	'image/jpeg',
-	'images/webp',
-];
+`;
 
 const Article = ({ text }) => {
+  const options = {
+    renderNode: {
+      [BLOCKS.HEADING_1]: (node) => <h1>{node.content[0].value}</h1>,
+      [BLOCKS.HEADING_2]: (node) => <h2>{node.content[0].value}</h2>,
+      [BLOCKS.HEADING_3]: (node) => <h3>{node.content[0].value}</h3>,
+      [BLOCKS.PARAGRAPH]: (node, children) => <p>{children}</p>,
+      [BLOCKS.EMBEDDED_ASSET]: (node) => (
+        <img
+          src={node.data.target.fields.file.url}
+          alt={node.data.target.fields.description}
+        />
+      ),
+      [BLOCKS.EMBEDDED_ENTRY]: (node) => (
+        <ArticleCard
+          targetArticle={node}
+        />
+      ),
+      [INLINES.ASSET_HYPERLINK]: (node) => (
+        <a
+          href={node.data.target.fields.file.url}
+        >
+          {node.content[0].value}
+        </a>
+      ),
+    },
+  };
 
-	const options = {
-		renderNode: {
-			[BLOCKS.HEADING_1]: (node) => {
-				return <h1>{node.content[0].value}</h1>
-			},
-			[BLOCKS.HEADING_2]: (node) => {
-				return <h2>{node.content[0].value}</h2>
-			},
-			[BLOCKS.HEADING_3]: (node) => {
-				return <h3>{node.content[0].value}</h3>
-			},
-			[BLOCKS.PARAGRAPH]: (node, children) => {
-				return <p>{children}</p>
-			},
-			[BLOCKS.EMBEDDED_ASSET]: (node) => {
-				return <img 
-					src={node.data.target.fields.file.url} 
-					alt={node.data.target.fields.description}
-				/>
-			},
-			[BLOCKS.EMBEDDED_ENTRY]: (node) => {
-				return <ArticleCard 
-					targetArticle={node}
-				/>
-			},
-			[INLINES.ASSET_HYPERLINK]: (node, children) => {
-				return <a href={node.data.target.fields.file.url}>{node.content[0].value}</a>
-			},
+  return (
+    <ArticleStyles>
+      {documentToReactComponents(text.fields.content, options)}
+    </ArticleStyles>
+  );
+};
 
-		},
-	};
+Article.defaultProps = {
+  text: {},
+};
 
-
-	return (
-		<ArticleStyles>
-			{documentToReactComponents(text.fields.content, options)}
-		</ArticleStyles>
-	)
+Article.propTypes = {
+  text: PropTypes.shape({
+    fields: PropTypes.shape({
+      content: PropTypes.shape.isRequired,
+    }),
+  }),
 };
 
 export default Article;
