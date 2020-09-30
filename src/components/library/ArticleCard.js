f5283a0af9ad8643b2246dc05060b3cb3e4a0a99
GummiCode <gummicodeblog@gmail.com>
2020-09-30 19:29:38 +0100

    Extracted aspects of the ArticleCard component to create a new component, LinkCard, which is a reuseable template for a card which is a link.

--- src/components/library/ArticleCard.js
+++ src/components/library/ArticleCard.js
@@ -1,40 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import LinkCard from './LinkCard';
 
-const StyledArticleCard = styled(Link)`
-  width: calc(100vw - 20px);
-  margin: 10px 10px 20px 10px;
-  max-width: 300px;
-  background: ${(props) => props.theme.cardBackground};
-  border: 2px solid ${(props) => props.theme.cardBorder};
-  border-radius: 5px;
-  color: ${(props) => props.theme.cardText};
-  display: flex;
-  flex-flow: column nowrap;
-
-  
-  &:link {
-    color: ${(props) => props.theme.cardHeadingText};
-    text-decoration: none;
-  }
-
-  &:visited{
-    color: ${(props) => props.theme.cardHeadingText};
-    text-decoration: none;
-  }
-
-  &:active {
-    color: ${(props) => props.theme.cardHeadingText};
-    text-decoration: none;
-  }
-    
-  &:hover{
-    color: ${(props) => props.theme.cardHeadingText};
-    text-decoration: none;
-  }
-
+const StyledArticleCard = styled(LinkCard)`
   .articleCardImage {
     padding: 0;
     margin: 0;
