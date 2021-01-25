292e052afbd9d06002bbd398e96605ffa1634aa4
Ian King <ian.antking@gmail.com>
2021-01-25 23:18:56 +0000

    add sponsor logos to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import SocialsCard from './socials-card';
 import { Link } from 'react-router-dom';
+import sponsors from '../utils/sponsors';
 
 const Footer = (props) => {
   const { pathname } = props;
+  console.log(sponsors)
   return (
     <footer>
       { pathname !== '/contact' ? (
-        <SocialsCard
-          facebook="https://www.facebook.com/Queer-Family-Tea-638643326624464"
-          instagram="https://www.instagram.com/queerfamilytea/"
-          email="mailto:info@queerfamilytea.com"
-        />
+        <React.Fragment>
+          <SocialsCard
+            facebook="https://www.facebook.com/Queer-Family-Tea-638643326624464"
+            instagram="https://www.instagram.com/queerfamilytea/"
+            email="mailto:info@queerfamilytea.com"
+          />
+          <div style={{ display: 'flex',  alignItems: 'center', justifyContent: 'space-between', margin: '10px', flexWrap: 'wrap' }}>
+          {
+            sponsors.map(sponsor => (
+              <div key={sponsor}>
+                <img style={{ maxWidth: '10vw' }} src={sponsor}></img>
+              </div>
+            ))
+          }
+        </div>
+        </React.Fragment>
       ) : (
         null
       )
---
+++ src/utils/sponsors.js
@@ -0,0 +1,9 @@
+export default [
+    "https://images.ctfassets.net/yq2ab76w5obt/2rim31DVKsxR9aDsuGXunE/9f1e756c910f83b622063c5c251f2402/CR_Logo_Red_RGB.png",
+    "https://images.ctfassets.net/yq2ab76w5obt/7vtIAobZ2ZIQAR8TFis0Vv/0bf4f1768ff614d194845f7d43c6438c/Metro_Charity.jpg",
+    "https://images.ctfassets.net/yq2ab76w5obt/1z4lGWKKUTAwQAjaH3ZWqs/7025d27815bd67d1c610d1f6ce800d6d/Birmingham_LGBT_Logo.jpg",
+    "https://images.ctfassets.net/yq2ab76w5obt/4YeY2CdFZYKu5lOJygeJqq/aa70f8ea434100b7638c86581fb04a72/Manchester_Wellbeing_Fund.png",
+    "https://images.ctfassets.net/yq2ab76w5obt/24tReXMdG4dXrtwLcZP9Ek/b2e9fe6f8120dc606fa9d3158bcad878/NHS.png",
+    "https://images.ctfassets.net/yq2ab76w5obt/7MzTUeXvZrj74VDKxeNRvk/998480894881e7a26a69675f6af6943b/Yorkshire_MESMAC_Logo.jpg",
+    "https://images.ctfassets.net/yq2ab76w5obt/1ESWwvV7CLDFjCWrqEbTsp/993f9beb8d17e29c42c5bcb5e088f8ce/Superbia_fb-3.jpg"
+]
